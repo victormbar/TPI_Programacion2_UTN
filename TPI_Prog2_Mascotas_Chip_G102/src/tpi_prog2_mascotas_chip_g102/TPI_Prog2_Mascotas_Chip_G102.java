accea96ac713fd64602ad56e2eee85e78075d0d5
Victormanuel.bar <victormanuel.bar28@gmail.com>
2025-11-10 20:51:05 -0300

    Se crea carpeta de TPI
    Se realiza creación de TPI para compartirlo con los demas integrantes del equipo

---
+++ TPI_Prog2_Mascotas_Chip_G102/src/tpi_prog2_mascotas_chip_g102/TPI_Prog2_Mascotas_Chip_G102.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package tpi_prog2_mascotas_chip_g102;
+
+/**
+ *
+ * @author vcman
+ */
+public class TPI_Prog2_Mascotas_Chip_G102 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
