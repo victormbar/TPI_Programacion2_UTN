c75a1eaeba53f3a146855e0c826654409b22ca0a
Benitez, Rodrigo Ivan <rodrigoivanb18@gmail.com>
2025-11-11 01:00:22 -0300

    feat: agregar getters, setters y toString en Mascota

--- TPI_Prog2_Mascotas_Chip_G102/src/entities/Mascota.java
+++ TPI_Prog2_Mascotas_Chip_G102/src/entities/Mascota.java
@@ -23,4 +23,93 @@ public class Mascota {
 
     public Mascota() {
     }
+    
+    public Mascota(Long id, Boolean eliminado, String nombre, String especie, String raza, LocalDate fechaNacimiento, String duenio, Microchip microchip) {
+        this.id = id;
+        this.eliminado = eliminado;
+        this.nombre = nombre;
+        this.especie = especie;
+        this.raza = raza;
+        this.fechaNacimiento = fechaNacimiento;
+        this.duenio = duenio;
+        this.microchip = microchip;
+    }
+
+    public Long getId() { 
+        return id;
+    }
+    
+    public void setId(Long id) { 
+        this.id = id;
+    }
+
+    public Boolean getEliminado() { 
+        return eliminado;
+    }
+    
+    public void setEliminado(Boolean eliminado) { 
+        this.eliminado = eliminado;
+    }
+
+    public String getNombre() { 
+        return nombre;
+    }
+    
+    public void setNombre(String nombre) { 
+        this.nombre = nombre;
+    }
+
+    public String getEspecie() { 
+        return especie;
+    }
+    
+    public void setEspecie(String especie) { 
+        this.especie = especie;
+    }
+
+    public String getRaza() { 
+        return raza;
+    }
+    
+    public void setRaza(String raza) { 
+        this.raza = raza;
+    }
+
+    public LocalDate getFechaNacimiento() { 
+        return fechaNacimiento;
+    }
+    
+    public void setFechaNacimiento(LocalDate fechaNacimiento) { 
+        this.fechaNacimiento = fechaNacimiento;
+    }
+
+    public String getDuenio() { 
+        return duenio;
+    }
+    
+    public void setDuenio(String duenio) { 
+        this.duenio = duenio;
+    }
+
+    public Microchip getMicrochip() { 
+        return microchip;
+    }
+    
+    public void setMicrochip(Microchip microchip) { 
+        this.microchip = microchip;
+    }
+
+    @Override
+    public String toString() {
+        return "Mascota{" +
+                "id=" + id +
+                ", eliminado=" + eliminado +
+                ", nombre='" + nombre + '\'' +
+                ", especie='" + especie + '\'' +
+                ", raza='" + raza + '\'' +
+                ", fechaNacimiento=" + fechaNacimiento +
+                ", duenio='" + duenio + '\'' +
+                ", microchip=" + microchip +
+                '}';
+    }
 }
